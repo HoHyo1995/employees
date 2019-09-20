42781e6db520a3df8b96c1f327ad64c138e5d988
GD3 <GD3@DESKTOP-98CTC1G>
2019-09-20 17:22:11 +0900

    selectEmployeesList라는 메소드를 생성했는데 limit의 값을 인수로 받어  제한된 갯수로 출력되는 메소드를 생성

--- employees/src/model/EmployeesDao.java
+++ employees/src/model/EmployeesDao.java
@@ -1,8 +1,50 @@
 package model;
-
+import java.util.*;
+import vo.*;
 import java.sql.*;
 
 public class EmployeesDao {
+	public List<Employees> selectEmployeesList(int limit) {
+		System.out.println("EmployeesDao.java에 selectEMployeesList에 인수로 사용된 limit의 값은"+limit);
+		//Employees타입의 변수를 여러개 받기 위해  배열 생성
+		List<Employees> list = new ArrayList<Employees>();
+		// 메소드 로딩에 쓸 변수 생성 및 초기화
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		// employees타입들을 전원 출력할 쿼리문 사용 + 너무많아서 몇개씩 출력할껀지 제한하기위해 limit사용
+		final String sql = "select emp_no, birth_date, first_name, last_name, gender, hire_date from employees limit ?";
+		// 예외구문을 사용하여 DB에 값을 set메소드를 사용하여 복사
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees", "root", "java1234");
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, limit);
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				Employees employees = new Employees();
+				employees.setEmpNo(rs.getInt("emp_no"));
+				employees.setBirthDate(rs.getString("birth_date"));
+				employees.setFirstName(rs.getString("first_name"));
+				employees.setLastName(rs.getString("last_name"));
+				employees.setGender(rs.getString("gender"));
+				employees.setHireDate(rs.getString("hire_date"));
+				list.add(employees);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+	// index에 사용할 employeesDB의 사원 수를 구하는 메소드
 	public int selectEmployeesCount() {
 		int count = 0;
 		final String sql = "select count(*) from employees";
