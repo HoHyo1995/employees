b6acf039bf4c19abe8759abd9cc23dcc262371b0
GD3 <GD3@DESKTOP-98CTC1G>
2019-09-20 17:42:40 +0900

    limit변수생성하고 리퀘스트를 이용해서 볼 수있는 데이터의 수를 지정 및 Dao에서 받은 메소드의 리턴값 리퀘스트에 저장

---
+++ employees/src/controller/GetEmployeesListServlet.java
@@ -0,0 +1,35 @@
+package controller;
+import model.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+import vo.*;
+
+
+@WebServlet("/employees/getEmployeesList")
+public class GetEmployeesListServlet extends HttpServlet {
+	private EmployeesDao employeesDao;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 한번에 출력할 갯수를 정하기 위해 limit이란 변수를 생성
+		int limit = 10;
+		// 한번에 출력할 갯수를 웹에서 정할 수 있게 하기위해서 사용 select태그 사용후 request로 받아올 예정
+		if(request.getParameter("limit") != null) {
+			Integer.parseInt(request.getParameter("limit"));
+		}
+		// EmployeesDao에있는 ,...list메소드를 사용하기위해 객체를 선언한다
+		 employeesDao = new EmployeesDao();
+		// return값이 ArrayList<Employees>라서 new연산자를 사용하지 않아도 된다.
+		List<Employees> list = employeesDao.selectEmployeesList(limit);
+		// request에 list라는 이름으로 list값을 보낸다
+		request.setAttribute("list", list);
+		// 포워드 ㄱㄱ
+		request.getRequestDispatcher("/WEB-INF/views/employees/employeesList.jsp").forward(request, response);
+	}
+
+
+
+}
