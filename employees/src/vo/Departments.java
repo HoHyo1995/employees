10207c9a2d08136cd2ae00d5e22b5f5c32a77860
GD3 <GD3@DESKTOP-98CTC1G>
2019-09-20 16:17:15 +0900

    vo패키지 안에 Departments라는 클래스 타입 생성

---
+++ employees/src/vo/Departments.java
@@ -0,0 +1,24 @@
+package vo;
+
+public class Departments {
+	// 필드 생성
+	private String deptNo;
+	private String deptName;
+	// 생성자 생성
+	public Departments() {
+		
+	}
+	// get,set메소드 생성
+	public String getDeptNo() {
+		return deptNo;
+	}
+	public void setDeptNo(String deptNo) {
+		this.deptNo = deptNo;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+}
