8170873b44ce2d5841a70f37da6188f948f05b34
GD3 <GD3@DESKTOP-98CTC1G>
2019-09-23 16:30:52 +0900

    TitlesDao 클래스를 생성 후 titles행의 수를 구하는 메소드 생성

---
+++ employees/src/model/TitlesDao.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class TitlesDao {
+	// 생성자 생성
+	public TitlesDao() {
+		
+	}
+		// 테이블의 행의 수 구하는 메소드
+		public int selectTitlesCount() {
+			int count = 0;
+			final String sql = "select count(*) from titles";
+			Connection conn = null;
+			PreparedStatement stmt = null;
+			ResultSet rs = null;
+			
+			try {
+				Class.forName("org.mariadb.jdbc.Driver");
+				conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees", "root", "java1234");
+				stmt = conn.prepareStatement(sql);
+				rs = stmt.executeQuery();
+				if(rs.next()) {
+					count = rs.getInt("count(*)");
+					
+				}
+			// Exception 예외의 최상위 클래스
+			}catch(Exception e) { // e라는 변수가  2개인 이유는 자바의 변수 생명주기는 {}
+				e.printStackTrace();
+			} finally {
+				try {
+					rs.close();
+					stmt.close();
+					conn.close();
+				}catch(Exception e) {
+					// 예외가 나면 콘솔창에 출력을 하는 메소드
+					e.printStackTrace();
+				}
+			}
+			return count;
+		}
+}
