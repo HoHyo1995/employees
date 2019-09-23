726bc93eaba4e9a2609c41242fb2bb9cfe7853bb
GD3 <GD3@DESKTOP-98CTC1G>
2019-09-23 17:13:19 +0900

    오름차순이랑내림차순이 값에따라 바뀌는 메소드 생성

--- employees/src/model/EmployeesDao.java
+++ employees/src/model/EmployeesDao.java
@@ -4,6 +4,58 @@ import vo.*;
 import java.sql.*;
 
 public class EmployeesDao {
+	// 생성자생성
+	public EmployeesDao(){
+		
+	}
+	// 전체 리스트를 출력하는 메소드인데 오름차순 내림차순이 있음 50개씩 보여주는 메소드
+	public List<Employees> selectEmployeesOrderBy(String order) {
+		System.out.println("EmployeesDao.java에 selectEMployeesList에 인수로 사용된 order의 값은"+order);
+		//Employees타입의 변수를 여러개 받기 위해  배열 생성
+		List<Employees> list = new ArrayList<Employees>();
+		// 메소드 로딩에 쓸 변수 생성 및 초기화
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		// 쿼리문이 오름차순이냐 내림차순이냐에 따라 바뀌므로 동적쿼리를 사용하엿따.
+		String sql = null;
+		if(order.equals("asc")) {
+			sql = "select emp_no, birth_date, first_name, last_name, gender, hire_date from employees order by first_name asc limit=50";
+		} else if(order.equals("desc")) {
+			sql = "select emp_no, birth_date, first_name, last_name, gender, hire_date from employees order by first_name desc limit=50";
+		}
+		
+		// 예외구문을 사용하여 DB에 값을 set메소드를 사용하여 복사
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees", "root", "java1234");
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				Employees employees = new Employees();
+				employees.setEmpNo(rs.getInt("emp_no"));
+				employees.setBirthDate(rs.getString("birth_date"));
+				employees.setFirstName(rs.getString("first_name"));
+				employees.setLastName(rs.getString("last_name"));
+				employees.setGender(rs.getString("gender"));
+				employees.setHireDate(rs.getString("hire_date"));
+				list.add(employees);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+	
+	// 전체 리스트를 출력하는 메소드
 	public List<Employees> selectEmployeesList(int limit) {
 		System.out.println("EmployeesDao.java에 selectEMployeesList에 인수로 사용된 limit의 값은"+limit);
 		//Employees타입의 변수를 여러개 받기 위해  배열 생성
