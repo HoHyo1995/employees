dc33c3bfb8a948f05503321512ab8858f92b5236
GD3 <GD3@DESKTOP-98CTC1G>
2019-09-23 16:09:57 +0900

    클래스 생성 후 행의 수를 알기 위한 메소드 생성

--- employees/src/model/DepartmentsDao.java
+++ employees/src/model/DepartmentsDao.java
@@ -14,6 +14,39 @@ public class DepartmentsDao {
 	public DepartmentsDao() {
 	 
  }
+	// index에 사용할 employeesDB의 부서 수를 구하는 메소드
+		public int selectDepartmentsCount() {
+			int count = 0;
+			final String sql = "select count(*) from departments";
+			Connection conn = null;
+			PreparedStatement stmt = null;
+			ResultSet rs = null;
+			
+			try {
+				Class.forName("org.mariadb.jdbc.Driver");
+				conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees", "root", "java1234");
+				stmt = conn.prepareStatement(sql);
+				rs = stmt.executeQuery();
+				if(rs.next()) {
+					count = rs.getInt("count(*)");
+					
+				}
+			// Exception 예외의 최상위 클래스
+			}catch(Exception e) { // e라는 변수가  2개인 이유는 자바의 변수 생명주기는 {}
+				e.printStackTrace();
+			} finally {
+				try {
+					rs.close();
+					stmt.close();
+					conn.close();
+				}catch(Exception e) {
+					// 예외가 나면 콘솔창에 출력을 하는 메소드
+					e.printStackTrace();
+				}
+			}
+			return count;
+		}
+	
 	// 부서들의 이름과 부서번호를 리스트로 출력하는 메소드
 	public List<Departments> selectDepartmentsList() {
 		// Departments타입을 여러개 받기 위해 List<>배열 생성한다.
