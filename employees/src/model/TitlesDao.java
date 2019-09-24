b934bc4e832666d09ea6db7f34456b87fbfed473
GD3 <GD3@DESKTOP-98CTC1G>
2019-09-24 18:15:41 +0900

    중복을 제거한 titles리스트 메소드 생성

--- employees/src/model/TitlesDao.java
+++ employees/src/model/TitlesDao.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.sql.Connection;
+import java.util.*;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,6 +13,32 @@ public class TitlesDao {
 	public TitlesDao() {
 		
 	}
+		//중복을 제거한 titles의 리스트
+	public List<String> selectTitlesListDistinct() {
+		// 리턴값인 List<String>타입을 선언한다.
+		List<String> list = new ArrayList<String>();
+		// 마리아 DB로딩에 필요한 변수를 선언하고 초기화한다
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		// 쿼리문을 String타입으로 변수 선언한다
+		String sql = "select distinct(title) from titles";
+		// getConnection메소드를 불러온후 마리아DB에값을 저장한다.
+		try {
+			conn = DBHelper.getConnection();
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				list.add(rs.getString("title"));
+			}
+		} catch(Exception e) {
+			
+		} finally {
+			DBHelper.close(rs, stmt, conn);
+		}
+		return list;
+	}
+	
 		// 테이블의 행의 수 구하는 메소드
 		public int selectTitlesCount() {
 			int count = 0;
