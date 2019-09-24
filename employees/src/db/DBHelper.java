ba2e9e1cb930efa4b75a8e73b2211111e9ddf794
GD3 <GD3@DESKTOP-98CTC1G>
2019-09-24 11:59:24 +0900

    클래스를 생성 후 마리아DB를 로딩해주는 메소드와 변수들을 close해주는 메소드를 생성했다.

---
+++ employees/src/db/DBHelper.java
@@ -0,0 +1,38 @@
+package db;
+import java.sql.*;
+public class DBHelper {
+	// 마리아DB 사용을 할때 중복된 부분을 메소드화 함
+	public static Connection getConnection() throws Exception {
+		Class.forName("org.mariadb.jdbc.Driver");
+		Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees", "root", "java1234");
+		return conn;
+	}
+	
+	// close메소드
+	public static void close(ResultSet rs, PreparedStatement stmt, Connection conn ) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
