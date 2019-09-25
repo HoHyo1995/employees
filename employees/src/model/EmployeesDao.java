cab303ede01137a672df9530f886c7b1af487966
GD3 <GD3@DESKTOP-98CTC1G>
2019-09-25 17:11:32 +0900

    페이징하여 출력하는 메소드와 마지막페이지의 값을 구하는 메소드를 생성

--- employees/src/model/EmployeesDao.java
+++ employees/src/model/EmployeesDao.java
@@ -10,6 +10,63 @@ public class EmployeesDao {
 	public EmployeesDao(){
 		
 	}
+	// 마지막페이지를 구하는 메소드
+	public int selectLastPage(int rowPerPage) {
+		// rowPerPage가 넘어오는지 확인한다.
+		System.out.println("rowPerPage = "+ rowPerPage);
+		// 마지막페이지를 담을 변수 생성 및 초기화 (if안에서 생성하면 지역변수가 되기때문에 if 밖에서 생성해서 리턴해준다)
+		int lastPage = 0;
+		// this.selectEmployeesCount()메소드가 총행을 구하는 메소드이므로 이용해서 마지막페이지를 구한다.
+		int totalRow = this.selectEmployeesCount();
+		// if문을 사용해서 행의 수에 따라 달라지는 마지막페이지를 구한다
+		if(totalRow % rowPerPage != 0) {
+			lastPage = (totalRow / rowPerPage)+1;
+		} else {
+			lastPage = totalRow / rowPerPage;
+		}
+		
+		return lastPage;
+	}
+	// 10개씩 페이징하여 출력한 메소드
+	public List<Employees> getEmployeesListByPage(int currentPage, int rowPerPage) {
+		// 인수로 넘어온값이 맞게 넘어왔는지 확인한다
+		System.out.println("currentPage = "+currentPage+"rowPerPage = "+rowPerPage);
+		// 리턴값에 맞는 변수 생성
+		List<Employees> list = new ArrayList<Employees>();
+		// 마리아DB로딩에 필요한 변수들을 선언하고 초기화한다.
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		// 쿼리문을 String타입에 복사한다
+		// limit은 0부터시작한다
+		final String sql = "select emp_no, birth_date, first_name, last_name, gender, hire_date from employees limit ?, ?";
+		
+		try {
+			// 마리아 DB 로딩 및 연결
+			conn = DBHelper.getConnection();
+			stmt = conn.prepareStatement(sql);
+			// 시작값을 currentPage로 하면 계속 시작페이지가 똑같아서 같은값이 나오게 되어서 startRow라는 변수를 생성했다.
+			int startRow = (currentPage-1)*rowPerPage;
+			stmt.setInt(1, startRow);
+			stmt.setInt(2, rowPerPage);
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				Employees employees = new Employees();
+				employees.setEmpNo(rs.getInt("emp_no"));
+				employees.setBirthDate(rs.getString("birth_date"));
+				employees.setFirstName(rs.getString("first_name"));
+				employees.setLastName(rs.getString("last_name"));
+				employees.setGender(rs.getString("gender"));
+				employees.setHireDate(rs.getString("hire_date"));
+				list.add(employees);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBHelper.close(rs, stmt, conn);
+		}
+		return list;
+	}
 	// begin이랑 end값을 받은 후 그 사이값을 출력하는 메소드
 	public List<Employees> selectEmployeesListBetween(int begin, int end){
 		// 동적배열 사용을위해 생성
