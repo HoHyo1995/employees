fa45fd2b2d6bb4d2605aa0c13bef61151afea300
GD3 <GD3@DESKTOP-98CTC1G>
2019-09-30 14:09:09 +0900

    로그인을 하지않고 index로 들어오면 로그인창으로 넘어가게 함

--- employees/src/controller/IndexServlet.java
+++ employees/src/controller/IndexServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet("/index")
@@ -20,6 +21,12 @@ public class IndexServlet extends HttpServlet {
 	private SalariesDao salariesDao;
 	private TitlesDao titlesDao;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 로그인 
+		HttpSession session = request.getSession();
+		if(session.getAttribute("sessionEmpNo") == null) {
+			response.sendRedirect(request.getContextPath()+"/login");
+			return ;
+		}
 		// 객체를 생성
 		this.employeesDao = new EmployeesDao();
 		this.departmentsDao = new DepartmentsDao();
