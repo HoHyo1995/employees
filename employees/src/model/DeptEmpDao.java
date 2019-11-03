87d79933dc1815658f832772c63a7201594a399e
wewe1 <wewe1@DESKTOP-5S1ABLU>
2019-11-03 23:16:15 +0900

    이너 조인을 사용하여 select를 사용한 메소드 생성

--- employees/src/model/DeptEmpDao.java
+++ employees/src/model/DeptEmpDao.java
@@ -9,11 +9,56 @@ import java.util.List;
 
 import db.DBHelper;
 import vo.Departments;
+import vo.DeptEmp;
+import vo.Employees;
 
 public class DeptEmpDao {
 	//생성자 생성
 	public DeptEmpDao() {
 		
+	}
+	// 사원 정보를 나타내는 메소드
+	public List<DeptEmp> getDeptEmpList(int currentPage, int rowPerPage){
+		// 넘어 온 인수를 확인한다
+		System.out.println("현재페이지 : "+currentPage+"페이지당 행의 수 : "+rowPerPage);
+		// 시작값을 정한다
+		int startRow = (currentPage-1)*rowPerPage;
+		// 리턴받을 값을 변수로 선언한다
+		List<DeptEmp> list = new ArrayList<DeptEmp>();
+		// 쿼리문을 변수에 저장
+		final String sql = "SELECT de.dept_no, d.dept_name, de.emp_no, e.first_name, e.last_name, de.to_date \r\n" + 
+				"FROM dept_emp de INNER JOIN employees e INNER JOIN departments d\r\n" + 
+				"ON de.emp_no = e.emp_no AND de.dept_no = d.dept_no\r\n" + 
+				"LIMIT ?, ?";
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DBHelper.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, startRow);
+			stmt.setInt(2, rowPerPage);
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				DeptEmp deptEmp = new DeptEmp();
+				deptEmp.setDepartments(new Departments());
+				deptEmp.getDepartments().setDeptNo(rs.getString("de.dept_no"));
+				deptEmp.getDepartments().setDeptName(rs.getString("d.dept_name"));
+				deptEmp.setEmployees(new Employees());
+				deptEmp.getEmployees().setEmpNo(rs.getInt("de.emp_no"));
+				deptEmp.getEmployees().setFirstName(rs.getString("e.first_name"));
+				deptEmp.getEmployees().setLastName("e.last_name");
+				deptEmp.setToDate(rs.getString("de.to_date"));
+				list.add(deptEmp);
+			}
+		// Exception 예외의 최상위 클래스
+		}catch(Exception e) { // e라는 변수가  2개인 이유는 자바의 변수 생명주기는 {}
+			e.printStackTrace();
+		} finally {
+			DBHelper.close(rs, stmt, conn);
+		}
+		return list;
 	}
 	// 사원별로 해당 부서를 나타내는 테이블의 행의 수 구하는 메소드
 				public int selectDeptEmpCount() {
